9bff431ed4322625ede089a9f570902318003616
Michal Zagrodzki <michalzagrodzki@gmail.com>
2021-06-03 17:43:11 +0200

    Add service call for details

---
+++ src/service/details.js
@@ -0,0 +1,17 @@
+import { Instance } from "./index";
+
+export const getDetails = (payload) =>
+  Instance({
+    method: "GET",
+    url: `/details/${payload.id}`,
+    transformResponse: [
+      (data) => {
+        try {
+          const response = JSON.parse(data);
+          return response;
+        } catch (error) {
+          throw Error(error);
+        }
+      },
+    ],
+  });
