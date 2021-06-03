39aa867c21014b01325ba83e52e1c6acb90e8311
Michal Zagrodzki <michalzagrodzki@gmail.com>
2021-06-03 17:42:54 +0200

    Add mockup for details

---
+++ src/mockup/item.js
@@ -0,0 +1,44 @@
+const Item = {
+  id: 12,
+  title: "Pasta",
+  price: "5.49",
+  weight: 200,
+  calories: 500,
+  ingredients: [
+    {
+      name: "eggs",
+    },
+    {
+      name: "flour",
+    },
+    {
+      name: "olive oil",
+    },
+    {
+      name: "salt",
+    },
+    {
+      name: "beef",
+    },
+    {
+      name: "onion",
+    },
+    {
+      name: "basil",
+    },
+    {
+      name: "oregano",
+    },
+    {
+      name: "tomato sauce",
+    },
+    {
+      name: "salt",
+    },
+    {
+      name: "cheddar cheeese",
+    },
+  ],
+};
+
+export { Item };
