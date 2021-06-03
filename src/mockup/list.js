6d05c4f3eb6275a9f13b54b1f808d68660c39888
Michal Zagrodzki <michalzagrodzki@gmail.com>
2021-06-03 12:40:07 +0200

    Add mockup data

---
+++ src/mockup/list.js
@@ -0,0 +1,22 @@
+const List = [
+  {
+    title: "Pasta",
+    price: "5.49",
+    weight: 200,
+    calories: 500,
+  },
+  {
+    title: "Pizza",
+    price: "6.25",
+    weight: 180,
+    calories: 450,
+  },
+  {
+    title: "Risotto",
+    price: "4.70",
+    weight: 160,
+    calories: 400,
+  },
+];
+
+export { List };
