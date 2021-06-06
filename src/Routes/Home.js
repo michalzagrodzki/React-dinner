b81dc949e4053803846fe58aad4263a2d6bd0280
Michal Zagrodzki <michalzagrodzki@gmail.com>
2021-06-06 21:33:00 +0200

    Update Home view

--- src/Routes/Home.js
+++ src/Routes/Home.js
@@ -1,27 +1,55 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { getList } from "./../service/list";
+import { ListItems as ItemsList } from "./../components/List/listItems";
+import { List as Items } from "./../mockup/list";
 import logo from "./../assets/logo.svg";
 import "./../styles/Home.css";
 
 class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true,
+      list: null,
+    };
+  }
+
   render() {
+    const { list } = this.state;
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+          <p>React dinner app.</p>
+          <ItemsList items={list} />
+          <Link
+            to={{
+              pathname: "/form",
+            }}
           >
-            Learn React
-          </a>
+            Link to form
+          </Link>
         </header>
       </div>
     );
+  }
+
+  async componentDidMount() {
+    try {
+      const response = await getList();
+      const shortResponse = response.data.slice(0, 2);
+      this.setState({
+        list: shortResponse,
+      });
+    } catch (error) {
+      console.log(`Axios request failed: ${error}`);
+    } finally {
+      this.setState({
+        isLoading: false,
+        list: Items.slice(0, 3),
+      });
+    }
   }
 }
 
