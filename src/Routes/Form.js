db8e1d1151ef4fd28e483c9a8633e4147438bdeb
Michal Zagrodzki <michalzagrodzki@gmail.com>
2021-06-10 22:02:54 +0200

    Create outline for form validation methods

--- src/Routes/Form.js
+++ src/Routes/Form.js
@@ -1,6 +1,14 @@
 import React from "react";
 import { postCustomOrder } from "./../service/orders";
 import { Container } from "../components/Form/container.js";
+import {
+  validateName,
+  validateEmail,
+  validatePhone,
+  validateWeight,
+  validateCalories,
+  validateIngredients,
+} from "../utils/formValidation";
 import "./../styles/Home.css";
 
 class Form extends React.Component {
@@ -14,6 +22,8 @@ class Form extends React.Component {
       weight: null,
       calories: null,
       ingredients: null,
+      errors: {},
+      submitError: null,
     };
   }
 
@@ -22,18 +32,58 @@ class Form extends React.Component {
     return state;
   }
 
+  isErrors(state) {
+    return false;
+  }
+
   async handleSubmit() {
     const filteredState = this.sweepState(this.state);
-    const payload = {
-      body: filteredState,
-    };
-    await postCustomOrder(payload);
+    if (!this.isErrors(filteredState)) {
+      this.setState({ submitError: null });
+      const payload = {
+        body: filteredState,
+      };
+      await postCustomOrder(payload);
+    } else {
+      this.setState({ submitError: "Please check input errors" });
+    }
+  }
+  validateInput(type, value) {
+    let errors = this.state.errors;
+    switch (type) {
+      case "name":
+        errors = { ...errors, name: validateName(value) };
+        break;
+      case "email":
+        errors = { ...errors, email: validateEmail(value) };
+        break;
+      case "phone":
+        errors = { ...errors, phone: validatePhone(value) };
+        break;
+      case "weight":
+        errors = { ...errors, weight: validateWeight(value) };
+        break;
+      case "calories":
+        errors = { ...errors, calories: validateCalories(value) };
+        break;
+      case "ingredients":
+        errors = { ...errors, ingredients: validateIngredients(value) };
+        break;
+      default:
+        break;
+    }
+    if (Object.entries(errors).length > 0) {
+      this.setState({ errors: errors });
+      console.log(this.state);
+      return;
+    }
   }
   handleInputChange(event) {
     event.preventDefault();
-    this.setState({
-      [event.target.name]: event.target.value,
-    });
+    const key = event.target.name;
+    const value = event.target.value;
+    this.validateInput(key, value);
+    this.setState({ [key]: value });
   }
 
   render() {
---
+++ src/utils/formValidation.js
@@ -0,0 +1,23 @@
+export const validateName = (input) => {
+  return "error";
+};
+
+export const validateEmail = (input) => {
+  return "error";
+};
+
+export const validatePhone = (input) => {
+  return "error";
+};
+
+export const validateWeight = (input) => {
+  return "error";
+};
+
+export const validateCalories = (input) => {
+  return "error";
+};
+
+export const validateIngredients = (input) => {
+  return "error";
+};
