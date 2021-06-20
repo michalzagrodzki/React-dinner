326692881d484f2d2b42c5d2516a93e1b4f43b6b
Michal Zagrodzki <michalzagrodzki@gmail.com>
2021-06-20 20:45:23 +0200

    Add id property to mockup list

--- src/mockup/list.js
+++ src/mockup/list.js
@@ -1,17 +1,20 @@
 const List = [
   {
+    id: 1,
     title: "Pasta",
     price: "5.49",
     weight: 200,
     calories: 500,
   },
   {
+    id: 2,
     title: "Pizza",
     price: "6.25",
     weight: 180,
     calories: 450,
   },
   {
+    id: 3,
     title: "Risotto",
     price: "4.70",
     weight: 160,
