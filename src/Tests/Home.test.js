041a936382ba012c03a2e302eb1edf5950a55039
Michal Zagrodzki <michalzagrodzki@gmail.com>
2021-07-14 19:09:00 +0200

    Replace hardcoded strings with references to labels in Home test

--- src/Tests/Home.test.js
+++ src/Tests/Home.test.js
@@ -1,9 +1,11 @@
+/* eslint-disable max-lines-per-function */
 import { render, screen, waitFor } from "@testing-library/react";
 import React from "react";
 import Home from "./../routes/Home";
 import { MemoryRouter } from "react-router-dom";
 import axios from "axios";
 import { List as mockList } from "./../mockup/list";
+import { HOME, LIST } from "./../utils/labels";
 
 jest.mock("axios");
 
@@ -12,14 +14,16 @@ describe("Home Page", () => {
     axios.get.mockResolvedValue({ data: mockList });
     test("renders app title", async () => {
       render(<Home />, { wrapper: MemoryRouter });
-      const linkElement = await waitFor(() => screen.getByText(/Dinner App/i));
+      const linkElement = await waitFor(() =>
+        screen.getByText(new RegExp(HOME.HEADER_LABEL, "i"))
+      );
       expect(linkElement).toBeInTheDocument();
     });
 
     test("renders list title", async () => {
       render(<Home />, { wrapper: MemoryRouter });
       const linkElement = await waitFor(() =>
-        screen.getByText(/Most popular dinners:/i)
+        screen.getByText(new RegExp(LIST.SUB_HEADER_LABEL, "i"))
       );
       expect(linkElement).toBeInTheDocument();
     });
@@ -27,7 +31,7 @@ describe("Home Page", () => {
     test("renders three items in list", async () => {
       render(<Home />, { wrapper: MemoryRouter });
       const linkElement = await waitFor(() =>
-        screen.getByText(/Most popular dinners:/i)
+        screen.getByText(new RegExp(LIST.SUB_HEADER_LABEL, "i"))
       );
       expect(linkElement).toBeInTheDocument();
     });
@@ -35,7 +39,7 @@ describe("Home Page", () => {
     test("renders link to full list", async () => {
       render(<Home />, { wrapper: MemoryRouter });
       const linkElement = await waitFor(() =>
-        screen.getByText(/View all dinners/i)
+        screen.getByText(new RegExp(LIST.LINK_LABEL, "i"))
       );
       expect(linkElement).toBeInTheDocument();
     });
@@ -45,14 +49,16 @@ describe("Home Page", () => {
     axios.get.mockResolvedValue({ data: [] });
     test("renders app title", async () => {
       render(<Home />, { wrapper: MemoryRouter });
-      const linkElement = await waitFor(() => screen.getByText(/Dinner App/i));
+      const linkElement = await waitFor(() =>
+        screen.getByText(new RegExp(HOME.HEADER_LABEL, "i"))
+      );
       expect(linkElement).toBeInTheDocument();
     });
 
     test("renders list title", async () => {
       render(<Home />, { wrapper: MemoryRouter });
       const linkElement = await waitFor(() =>
-        screen.getByText(/Most popular dinners:/i)
+        screen.getByText(new RegExp(LIST.SUB_HEADER_LABEL, "i"))
       );
       expect(linkElement).toBeInTheDocument();
     });
@@ -69,7 +75,7 @@ describe("Home Page", () => {
     test("renders title to link to custom order form", async () => {
       render(<Home />, { wrapper: MemoryRouter });
       const linkElement = await waitFor(() =>
-        screen.getByText(/Haven't found what you were looking for?/i)
+        screen.getByText(new RegExp(HOME.FORM_LINK_CAPTION, "i"))
       );
       expect(linkElement).toBeInTheDocument();
     });
@@ -78,7 +84,7 @@ describe("Home Page", () => {
       axios.get.mockResolvedValue({ data: mockList });
       render(<Home />, { wrapper: MemoryRouter });
       const linkElement = await waitFor(() =>
-        screen.getByText(/Place a custom order/i)
+        screen.getByText(new RegExp(HOME.FORM_LINK_LABEL, "i"))
       );
       expect(linkElement).toBeInTheDocument();
     });
