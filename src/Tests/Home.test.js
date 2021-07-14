f3d7f90f86faec6d5e32ab84624b468dbc13ce1c
Michal Zagrodzki <michalzagrodzki@gmail.com>
2021-07-14 18:30:28 +0200

    Specify tests and methods in Home test component

--- src/Tests/Home.test.js
+++ src/Tests/Home.test.js
@@ -1,29 +1,85 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import React from "react";
 import Home from "./../routes/Home";
 import { MemoryRouter } from "react-router-dom";
+import axios from "axios";
+import { List as mockList } from "./../mockup/list";
+
+jest.mock("axios");
 
 describe("Home Page", () => {
   describe("Happy path", () => {
-    test("renders learn react link", () => {
+    axios.get.mockResolvedValue({ data: mockList });
+    test("renders app title", async () => {
       render(<Home />, { wrapper: MemoryRouter });
-      const linkElement = screen.getByText(/Dinner App/i);
+      const linkElement = await waitFor(() => screen.getByText(/Dinner App/i));
+      expect(linkElement).toBeInTheDocument();
+    });
+
+    test("renders list title", async () => {
+      render(<Home />, { wrapper: MemoryRouter });
+      const linkElement = await waitFor(() =>
+        screen.getByText(/Most popular dinners:/i)
+      );
+      expect(linkElement).toBeInTheDocument();
+    });
+
+    test("renders three items in list", async () => {
+      render(<Home />, { wrapper: MemoryRouter });
+      const linkElement = await waitFor(() =>
+        screen.getByText(/Most popular dinners:/i)
+      );
+      expect(linkElement).toBeInTheDocument();
+    });
+
+    test("renders link to full list", async () => {
+      render(<Home />, { wrapper: MemoryRouter });
+      const linkElement = await waitFor(() =>
+        screen.getByText(/View all dinners/i)
+      );
       expect(linkElement).toBeInTheDocument();
     });
   });
 
   describe("Missing list", () => {
-    test("renders learn react link", () => {
+    axios.get.mockResolvedValue({ data: [] });
+    test("renders app title", async () => {
       render(<Home />, { wrapper: MemoryRouter });
-      const linkElement = screen.getByText(/Dinner App/i);
+      const linkElement = await waitFor(() => screen.getByText(/Dinner App/i));
+      expect(linkElement).toBeInTheDocument();
+    });
+
+    test("renders list title", async () => {
+      render(<Home />, { wrapper: MemoryRouter });
+      const linkElement = await waitFor(() =>
+        screen.getByText(/Most popular dinners:/i)
+      );
+      expect(linkElement).toBeInTheDocument();
+    });
+
+    test("renders message of missing list", async () => {
+      render(<Home />, { wrapper: MemoryRouter });
+      const linkElement = screen.getByText(/No dinners visible/i);
       expect(linkElement).toBeInTheDocument();
     });
   });
 
   describe("Link to form", () => {
-    test("renders link to custom order form", () => {
+    axios.get.mockResolvedValue({ data: mockList });
+    test("renders title to link to custom order form", async () => {
       render(<Home />, { wrapper: MemoryRouter });
-      const linkElement = screen.getByText(/Dinner App/i);
+      const linkElement = await waitFor(() =>
+        screen.getByText(/Haven't found what you were looking for?/i)
+      );
+      expect(linkElement).toBeInTheDocument();
+    });
+
+    test("renders link to custom order form", async () => {
+      axios.get.mockResolvedValue({ data: mockList });
+      render(<Home />, { wrapper: MemoryRouter });
+      const linkElement = await waitFor(() =>
+        screen.getByText(/Place a custom order/i)
+      );
       expect(linkElement).toBeInTheDocument();
     });
   });
