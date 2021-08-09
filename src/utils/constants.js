c9d0f108aa9e019a10c96ab779f4de5c7fffbc69
Michal Zagrodzki <michalzagrodzki@gmail.com>
2021-08-09 16:56:15 +0200

    feat: create method for ommiting unwanted keys in table header

---
+++ src/utils/constants.js
@@ -0,0 +1,1 @@
+export const OMITTED_TABLE_KEYS = ["__v", "ingredients"];
--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,3 +1,10 @@
 export const shortenList = (list) => {
   return list.slice(0, 2);
 };
+
+export const removeSelectedItems = (list, keys) => {
+  const arrayWithoutOmittedKeys = list.filter((item) => {
+    return keys.indexOf(item) < 0 ? item : undefined;
+  });
+  return arrayWithoutOmittedKeys;
+};
